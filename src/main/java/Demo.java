fe249410a268f91020c5b38509e3e761d5f4459d
liu ze <ze.liu@happy-seed.com>
2021-01-19 18:51:42 +0800

    提交master分支

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,12 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("uat********************"); 
-        System.out.println("test----------------------");
-        System.out.println("dev====================");
-        System.out.println("master||||||||||||||||||||||||");
-        System.out.println("dev====================");
-        System.out.println("liuze|||||||||||||||||");
-        System.out.println("dev===================="); 
-        System.out.println("devops===================="); 
+        System.out.println("master********************");
     }
 }
