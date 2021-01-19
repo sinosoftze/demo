8161fab880b32883a1bd96a036a927776a70ca0a
liu ze <ze.liu@happy-seed.com>
2021-01-19 15:52:41 +0800

    test分支

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("test----------------------");
         System.out.println("dev====================");
         System.out.println("master||||||||||||||||||||||||");
         System.out.println("dev====================");
