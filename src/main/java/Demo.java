1875ef0e0d7039ccac3ba9f36f8b57545635a6ea
liu ze <ze.liu@happy-seed.com>
2021-01-19 15:48:23 +0800

    master分支为空

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,8 +1,4 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("dev====================");
-        System.out.println("master||||||||||||||||||||||||");
-        System.out.println("dev====================");
-        System.out.println("liuze===================");
     }
 }
