4fac856435c3779da451a73954c51fc32a6d0c17
liu ze <ze.liu@happy-seed.com>
2021-01-19 15:53:37 +0800

    uat分支

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("dev====================");
-        System.out.println("liuze|||||||||||||||||");
+        System.out.println("uat********************");
     }
 }
