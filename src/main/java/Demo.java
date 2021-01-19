e8ca4ab22999bb68decccefae6604476dc91603b
liu ze <ze.liu@happy-seed.com>
2021-01-19 19:07:02 +0800

    提交liuze方法

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("master********************");
-        System.out.println("dev提交代码");
         System.out.println("liuze提交代码");
     }
 
