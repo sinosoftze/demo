53732a3d5254652e1b8d1ef57799806b00e856ad
sinosoftze <34360406+sinosoftze@users.noreply.github.com>
2021-01-19 16:36:56 +0800

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,12 +1,11 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("uat********************");
+        System.out.println("uat********************"); 
         System.out.println("test----------------------");
         System.out.println("dev====================");
         System.out.println("master||||||||||||||||||||||||");
         System.out.println("dev====================");
         System.out.println("liuze|||||||||||||||||");
-        System.out.println("uat********************");
-        System.out.println("dev提交代码");
+        System.out.println("dev===================="); 
     }
 }
