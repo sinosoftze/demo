9ff88e847f6128424f2f8f7a1163681864ae7943
liu ze <ze.liu@happy-seed.com>
2021-01-19 15:15:32 +0800

    修改master分支

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,12 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("提交第二个版本");
-        System.out.println("提交第三个版本");
-        System.out.println("提交第四个版本");
-        System.out.println("dev分支提交");
-        System.out.println("提交dev代码");
-        System.out.println("提交刘泽代码");
-        System.out.println("修改刘泽分支");
-        System.out.println("修改dev分支");
+        System.out.println("master..............分支");
     }
 }
