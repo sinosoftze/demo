9e18ff6664235ce3f9c67a645c986bb9f5c7599f
liu ze <ze.liu@happy-seed.com>
2021-01-19 15:17:08 +0800

    修改刘泽分支

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("master..............分支");
+        System.out.println("liuZe||||||||||||||||||||");
     }
 }
